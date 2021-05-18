61199ae11f6ec2666e002169a26f66a4a3777a57
Rinatik79 <sabitovrinat@gmail.com>
2021-05-18 08:22:22 -0400

    Home work for Lesson 1

---
+++ lesson1.py
@@ -0,0 +1,43 @@
+# a = 7
+# b = 7.7
+# c = "Seventh son of the seventh son."
+# name = input("Enter your name please: ")
+# age = int(input("Enter your age please: "))
+# print("int a = " + str(a))
+# print("float b = " + str(b))
+# print("String c is '" + c + "'")
+# print("Your name is " + name)
+# print("Your age is " + str(age))
+#
+# total_seconds = int(input("Enter time in seconds please: "))
+# hours = 0
+# minutes = 0
+# seconds = 0
+# if total_seconds >= 60 ** 2:
+#     hours = total_seconds // 60 ** 2
+#     total_seconds = total_seconds % 60 ** 2
+# if total_seconds >= 60:
+#     minutes = total_seconds // 60
+#     seconds = total_seconds % 60
+# if hours < 10:
+#     hours = "0" + str(hours)
+# else:
+#     hours = str(hours)
+# if minutes < 10:
+#     minutes = "0" + str(minutes)
+# else:
+#     minutes = str(minutes)
+# if seconds < 10:
+#     seconds = "0" + str(seconds)
+# else:
+#     seconds = str(seconds)
+# print("You have entered time in seconds, counted as: " + hours + ":" + minutes + ":" + seconds)
+
+# basic_digit = input('Enter your digit: ')
+# second_number = int(basic_digit + basic_digit)
+# third_number = int(basic_digit + basic_digit + basic_digit)
+# basic_digit = int(basic_digit)
+# print("Sum:")
+# print((basic_digit + second_number + third_number))
+
+number = input("Enter number please")
