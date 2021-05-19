90a497ffb7ab79c56b64726198de830bd3f5eeb2
Rinatik79 <sabitovrinat@gmail.com>
2021-05-19 02:42:44 -0400

    Home work for Lesson 2

---
+++ lesson2.py
@@ -0,0 +1,3 @@
+megalist = [7, "Good", 7.7, True, [True, False, None, 1], (19, [0, 4]), {"Mama": "Elvira", "Papa": "Rinat"}, None]
+for one_member in megalist:
+    print("'" + str(one_member) + "' has type: " + str(type(one_member)))
