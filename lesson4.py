88dd4bdb600b639aa38b6692a2dcd7e7ecd6f92e
Rinatik79 <sabitovrinat@gmail.com>
2021-05-29 12:22:57 -0400

    Home work for Lesson 4-dev

---
+++ lesson4.py
@@ -0,0 +1,15 @@
+# Hi dear Maria!
+# Terminal doesn't want to work with russian text
+
+# Problem 1:
+# from sys import argv
+# working_time, working_rate, bonus = map(float, argv[1:])
+# print("Your pay is: " + str(working_time * working_rate + bonus))
+
+# Problem 2:
+# int_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+# result_list = [int_list[i] for i in range(1, len(int_list)) if int_list[i] > int_list[i-1]]
+# print(result_list)
+
+# Problem 3:
+
