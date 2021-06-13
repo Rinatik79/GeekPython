631a46ab1ce273a4accc6c229c7be9530293e5ac
rinatsabitov <sabitovrinat@gmail.com>
2021-06-13 17:43:58 -0400

    Lesson 8

--- lesson8.py
+++ lesson8.py
@@ -84,157 +84,169 @@
 
 # Problem 4,5,6:
 
-# current_warehouse_state = {"Printers": 0, "Scanners": 0, "Copiers": 0}
-# current_division_state = []
+# class OfficeEquipmentWarehouse:
+#     current_warehouse_state = {"Printers": 0, "Scanners": 0, "Copiers": 0}
+#     current_division_state = []
+#
+#     # def __init__(self):
+#     #     self.current_warehouse_state = {"Printers": 0, "Scanners": 0, "Copiers": 0}
+#     #     self.current_division_state = []
+#
+#     @staticmethod
+#     def convert_to_int(given_string):
+#         out = given_string
+#         try:
+#             out = int(given_string)
+#
+#         except ValueError:
+#             return "Enter int number please!"
+#
+#         else:
+#             return out
+#
+#     def division_with_name_index(self, division):
+#         for i in range(len(self.current_division_state)):
+#             if type(self.current_division_state[i].get(division)) == dict:
+#                 return i
+#
+#     def add_printers_to_warehouse(self, adding_qty):
+#         printers_to_add = OfficeEquipmentWarehouse.convert_to_int(adding_qty)
+#         if printers_to_add == "Enter int number please!":
+#             print(printers_to_add + "\n")
+#
+#         else:
+#             self.current_warehouse_state["Printers"] += printers_to_add
+#
+#     def add_scanners_to_warehouse(self, adding_qty):
+#         scanners_to_add = OfficeEquipmentWarehouse.convert_to_int(adding_qty)
+#         if scanners_to_add == "Enter int number please!":
+#             print(scanners_to_add + "\n")
+#
+#         else:
+#             self.current_warehouse_state["Scanners"] += scanners_to_add
+#
+#     def add_copiers_to_warehouse(self, adding_qty):
+#         copiers_to_add = OfficeEquipmentWarehouse.convert_to_int(adding_qty)
+#         if copiers_to_add == "Enter int number please!":
+#             print(copiers_to_add + "\n")
+#
+#         else:
+#             self.current_warehouse_state["Copiers"] += copiers_to_add
+#
+#     def send_printers_to_division(self, sending_qty, division):
+#         printers_to_send = OfficeEquipmentWarehouse.convert_to_int(sending_qty)
+#         if printers_to_send == "Enter int number please!":
+#             return print(printers_to_send + "\n")
+#
+#         if printers_to_send > self.current_warehouse_state.get("Printers"):
+#             print("Warehouse doesn't have asking qty: " + str(sending_qty) + "\n")
+#
+#         else:
+#             self.current_warehouse_state["Printers"] -= OfficeEquipmentWarehouse.convert_to_int(sending_qty)
+#             needed_division_position = self.division_with_name_index(division)
+#             if needed_division_position is None:
+#                 division_to_add = {division:
+#                                        {"Printers": printers_to_send,
+#                                         "Scanners": 0,
+#                                         "Copiers": 0}
+#                                    }
+#                 self.current_division_state.append(division_to_add)
+#             else:
+#                 new_printers_qty = self.current_division_state[needed_division_position].get(division).get(
+#                     "Printers") + printers_to_send
+#                 self.current_division_state[needed_division_position][division]["Printers"] = new_printers_qty
+#
+#     def send_scanners_to_division(self, sending_qty, division):
+#         scanners_to_send = OfficeEquipmentWarehouse.convert_to_int(sending_qty)
+#         if scanners_to_send == "Enter int number please!":
+#             return print(scanners_to_send + "\n")
+#
+#         if scanners_to_send > self.current_warehouse_state.get("Scanners"):
+#             print("Warehouse doesn't have asking qty: " + str(sending_qty) + "\n")
+#
+#         else:
+#             self.current_warehouse_state["Scanners"] -= OfficeEquipmentWarehouse.convert_to_int(sending_qty)
+#             needed_division_position = self.division_with_name_index(division)
+#             if needed_division_position is None:
+#                 division_to_add = {division:
+#                                        {"Printers": 0,
+#                                         "Scanners": scanners_to_send,
+#                                         "Copiers": 0}
+#                                    }
+#                 self.current_division_state.append(division_to_add)
+#             else:
+#                 new_scanners_qty = self.current_division_state[needed_division_position].get(division).get(
+#                     "Scanners") + scanners_to_send
+#                 self.current_division_state[needed_division_position][division]["Scanners"] = new_scanners_qty
+#
+#     def send_copiers_to_division(self, sending_qty, division):
+#         copiers_to_send = OfficeEquipmentWarehouse.convert_to_int(sending_qty)
+#         if copiers_to_send == "Enter int number please!":
+#             return print(copiers_to_send + "\n")
+#
+#         if copiers_to_send > self.current_warehouse_state.get("Copiers"):
+#             print("Warehouse doesn't have asking qty: " + str(sending_qty) + "\n")
+#
+#         else:
+#             self.current_warehouse_state["Copiers"] -= OfficeEquipmentWarehouse.convert_to_int(sending_qty)
+#             needed_division_position = self.division_with_name_index(division)
+#             if needed_division_position is None:
+#                 division_to_add = {division:
+#                                        {"Printers": 0,
+#                                         "Scanners": 0,
+#                                         "Copiers": copiers_to_send}
+#                                    }
+#                 self.current_division_state.append(division_to_add)
+#             else:
+#                 new_copiers_qty = self.current_division_state[needed_division_position].get(division).get(
+#                     "Copiers") + copiers_to_send
+#                 self.current_division_state[needed_division_position][division]["Copiers"] = new_copiers_qty
+#
+#
+# class OfficeEquipment(OfficeEquipmentWarehouse):
+#     pass
+#
+#
+# class Printer(OfficeEquipment):
+#     pass
+#
+#
+# class Scanner(OfficeEquipment):
+#     pass
+#
+#
+# class Copier(OfficeEquipment):
+#     pass
+#
+#
+# class Division(OfficeEquipmentWarehouse):
+#     pass
+#
+#
+# warehouse = OfficeEquipmentWarehouse()
+# warehouse.add_printers_to_warehouse("22")
+# warehouse.add_scanners_to_warehouse("33")
+# warehouse.add_copiers_to_warehouse(44)
+# warehouse.add_copiers_to_warehouse("MNOGO!!!")
+# print(warehouse.current_warehouse_state)
+# warehouse.send_scanners_to_division(77, "IT")
+# warehouse.send_scanners_to_division("7", "IT")
+# warehouse.send_printers_to_division("8", "IT")
+# warehouse.send_scanners_to_division("7", "IT")
+# warehouse.send_copiers_to_division("2", "IT")
+# warehouse.send_copiers_to_division("1", "IT")
+# print(warehouse.current_division_state)
+# print(warehouse.current_warehouse_state)
+#
+# warehouse.send_copiers_to_division("100", "bookkeeping")
+# warehouse.send_printers_to_division("8", "bookkeeping")
+# warehouse.send_scanners_to_division(3, "bookkeeping")
+# warehouse.send_copiers_to_division(10, "bookkeeping")
+# print(warehouse.current_division_state)
+# print(warehouse.current_warehouse_state)
+# warehouse.send_copiers_to_division("MNOGO!!!", "bookkeeping")
+# print(warehouse.current_division_state)
+# print(warehouse.current_warehouse_state)
 
-class OfficeEquipmentWarehouse:
-    current_warehouse_state = {"Printers": 0, "Scanners": 0, "Copiers": 0}
-    current_division_state = []
+# Problem 7:
 
-    # def __init__(self):
-    #     self.current_warehouse_state = {"Printers": 0, "Scanners": 0, "Copiers": 0}
-    #     self.current_division_state = []
-
-    @staticmethod
-    def convert_to_int(given_string):
-        out = given_string
-        try:
-            out = int(given_string)
-
-        except ValueError:
-            return "Enter int number please!"
-
-        else:
-            return out
-
-    def division_with_name_index(self, division):
-        for i in range(len(self.current_division_state)):
-            if type(self.current_division_state[i].get(division)) == dict:
-                return i
-
-    def add_printers_to_warehouse(self, adding_qty):
-        printers_to_add = OfficeEquipmentWarehouse.convert_to_int(adding_qty)
-        if printers_to_add == "Enter int number please!":
-            print(printers_to_add)
-
-        else:
-            self.current_warehouse_state["Printers"] += printers_to_add
-
-    def add_scanners_to_warehouse(self, adding_qty):
-        scanners_to_add = OfficeEquipmentWarehouse.convert_to_int(adding_qty)
-        if scanners_to_add == "Enter int number please!":
-            print(scanners_to_add)
-
-        else:
-            self.current_warehouse_state["Scanners"] += scanners_to_add
-
-    def add_copiers_to_warehouse(self, adding_qty):
-        copiers_to_add = OfficeEquipmentWarehouse.convert_to_int(adding_qty)
-        if copiers_to_add == "Enter int number please!":
-            print(copiers_to_add)
-
-        else:
-            self.current_warehouse_state["Copiers"] += copiers_to_add
-
-    def send_printers_to_division(self, sending_qty, division):
-        printers_to_send = OfficeEquipmentWarehouse.convert_to_int(sending_qty)
-        if printers_to_send == "Enter int number please!":
-            print(printers_to_send)
-
-        if printers_to_send > self.current_warehouse_state.get("Printers"):
-            print("Warehouse doesn't have asking qty: " + str(sending_qty))
-
-        else:
-            self.current_warehouse_state["Printers"] -= OfficeEquipmentWarehouse.convert_to_int(sending_qty)
-            needed_division_position = self.division_with_name_index(division)
-            if needed_division_position is None:
-                division_to_add = {division:
-                                       {"Printers": printers_to_send,
-                                        "Scanners": 0,
-                                        "Copiers": 0}
-                                   }
-                self.current_division_state.append(division_to_add)
-            else:
-                new_printers_qty = self.current_division_state[needed_division_position].get(division).get(
-                    "Printers") + printers_to_send
-                self.current_division_state[needed_division_position][division]["Printers"] = new_printers_qty
-
-    def send_scanners_to_division(self, sending_qty, division):
-        scanners_to_send = OfficeEquipmentWarehouse.convert_to_int(sending_qty)
-        if scanners_to_send == "Enter int number please!":
-            print(scanners_to_send)
-
-        if scanners_to_send > self.current_warehouse_state.get("Scanners"):
-            print("Warehouse doesn't have asking qty: " + str(sending_qty))
-
-        else:
-            self.current_warehouse_state["Scanners"] -= OfficeEquipmentWarehouse.convert_to_int(sending_qty)
-            needed_division_position = self.division_with_name_index(division)
-            if needed_division_position is None:
-                division_to_add = {division:
-                                       {"Printers": 0,
-                                        "Scanners": scanners_to_send,
-                                        "Copiers": 0}
-                                   }
-                self.current_division_state.append(division_to_add)
-            else:
-                new_scanners_qty = self.current_division_state[needed_division_position].get(division).get(
-                    "Scanners") + scanners_to_send
-                self.current_division_state[needed_division_position][division]["Scanners"] = new_scanners_qty
-
-    def send_copiers_to_division(self, sending_qty, division):
-        copiers_to_send = OfficeEquipmentWarehouse.convert_to_int(sending_qty)
-        if copiers_to_send == "Enter int number please!":
-            print(copiers_to_send)
-
-        if copiers_to_send > self.current_warehouse_state.get("Copiers"):
-            print("Warehouse doesn't have asking qty: " + str(sending_qty))
-
-        else:
-            self.current_warehouse_state["Copiers"] -= OfficeEquipmentWarehouse.convert_to_int(sending_qty)
-            needed_division_position = self.division_with_name_index(division)
-            if needed_division_position is None:
-                division_to_add = {division:
-                                       {"Printers": 0,
-                                        "Scanners": 0,
-                                        "Copiers": copiers_to_send}
-                                   }
-                self.current_division_state.append(division_to_add)
-            else:
-                new_copiers_qty = self.current_division_state[needed_division_position].get(division).get(
-                    "Copiers") + copiers_to_send
-                self.current_division_state[needed_division_position][division]["Copiers"] = new_copiers_qty
-
-
-class OfficeEquipment(OfficeEquipmentWarehouse):
-    pass
-
-
-class Printer(OfficeEquipment):
-    pass
-
-
-class Scanner(OfficeEquipment):
-    pass
-
-
-class Copier(OfficeEquipment):
-    pass
-
-
-class Division(OfficeEquipmentWarehouse):
-    pass
-
-
-warehouse = OfficeEquipmentWarehouse()
-warehouse.add_printers_to_warehouse("11")
-warehouse.add_scanners_to_warehouse("22")
-warehouse.add_copiers_to_warehouse("33")
-print(warehouse.current_warehouse_state)
-warehouse.send_scanners_to_division("7", "IT")
-warehouse.send_printers_to_division("8", "IT")
-warehouse.send_scanners_to_division("7", "IT")
-warehouse.send_copiers_to_division("2", "IT")
-warehouse.send_copiers_to_division("1", "IT")
-print(warehouse.current_division_state)
-print(warehouse.current_warehouse_state)
